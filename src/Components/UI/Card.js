dd260b8c1d70926ec001681ba3d9563cb187de17
Samadhan Maske <samadhanmaske208@gmail.com>
2023-11-04 17:55:22 +0530

    Added useState and validation for inputs handling

--- src/Components/UI/Card.js
+++ src/Components/UI/Card.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import classes from './Card.module.css';
 
 const Card = (props)=> {
-    console.log("Card.js");
+    
     return (
       <div className={`${classes.card} ${props.className}`}>
         {props.children}
--- src/Components/User/AddUser.js
+++ src/Components/User/AddUser.js
@@ -1,22 +1,39 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Card from "../UI/Card";
-import classes from './AddUser.module.css'
+import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 const AddUser = (props) => {
-    console.log("AddUser.js");
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
 
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" />
+        <input type="text" id="username" onChange={usernameChangeHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input type="number" id="username" />
+        <input type="number" id="username" onChange={ageChangeHandler} />
         {/* <button type="submit">Submit</button> */}
         <Button type="submit"> Add User </Button>
       </form>
